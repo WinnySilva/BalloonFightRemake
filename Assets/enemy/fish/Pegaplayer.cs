ae26d0feba1c0d0a7bc745b64b8f9a74db22efa4
selbacch <75805451+selbacch@users.noreply.github.com>
2021-06-12 20:03:01 -0300

    peixe comandos
    ao chegar perto da agua peixe levanta e ao chegar mais perto peixe agarra e leva para fundo do mar

--- Assets/enemy/fish/Pegaplayer.cs
+++ Assets/enemy/fish/Pegaplayer.cs
@@ -6,10 +6,11 @@ public class Pegaplayer : MonoBehaviour
 {
 
     public GameObject peixe;
-    public GameObject alvo;
+    public Transform alvo;
     public Animator anim;
     public Collider intercept;
-  
+    private bool caca;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,40 +21,110 @@ public class Pegaplayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         OnTriggerEnter(intercept);
         OnTriggerExit(intercept);
+        if (caca == true) {
+
+            hunt();
+        }
+
     }
 
     void OnTriggerEnter(Collider player)
-    {
-        
-            if (player.gameObject.tag == "Player")
-            {
-            
-
-            anim.SetBool("aparece",true);
-                    //peixe.SetActive(true);
-            // peixe.anim.SetBool("olhar", false);
-        }
-        
-    }
-
-
-    void OnTriggerExit(Collider player)
     {
 
         if (player.gameObject.tag == "Player")
         {
 
 
-            anim.SetBool("aparece", false);
+            anim.SetBool("aparece", true);
+            alvo = player.transform;
             //peixe.SetActive(true);
             // peixe.anim.SetBool("olhar", false);
+
+            caca = true;
         }
 
+      
+
+
+
+
+        if (player.gameObject.tag == "Enemy")
+            {
+
+
+                anim.SetBool("aparece", true);
+          
+            //peixe.SetActive(true);
+            // peixe.anim.SetBool("olhar", false);
+
+        }
+
+        }
+    
+        void MoveCharacter(Vector3 frameMovement)
+        {
+    transform.position += frameMovement;
+        }
+        void OnTriggerExit(Collider player)
+        {
+
+            if (player.gameObject.tag == "Player")
+            {
+
+
+                anim.SetBool("aparece", false);
+            alvo = null;
+            caca = false;
+                //peixe.SetActive(true);
+                // peixe.anim.SetBool("olhar", false);
+            }
+
+
+
+            if (player.gameObject.tag == "Enemy")
+            {
+
+
+                anim.SetBool("aparece", false);
+
+                //peixe.SetActive(true);
+                // peixe.anim.SetBool("olhar", false);
+            }
+
+
+
+        }
+
+    void hunt()   // caça o inimigo
+    {
+        Vector3 direction = alvo.position - transform.position;
+
+        direction.y = 0;
+        float distanceToTarget = direction.magnitude;
+
+        direction.Normalize();
+
+
+
+        Debug.Log(distanceToTarget);
+
+     
+
+
+
+
+
+
+
+        float distanceWantsToMoveThisFrame = 10000f * Time.deltaTime;
+        float actualMovementThisFrame = Mathf.Min(Mathf.Abs(distanceToTarget - 1), distanceWantsToMoveThisFrame);
+
+        MoveCharacter(actualMovementThisFrame * direction);
     }
 
-  
 
 
 }
--- Assets/enemy/fish/pegou.cs
+++ Assets/enemy/fish/pegou.cs
@@ -18,20 +18,31 @@ public class pegou : MonoBehaviour
     void Update()
     {
 
+        OnTriggerEnter(take);
 
-        void OnTriggerEnter(Collider player)
+
+    }
+
+
+    void OnTriggerEnter(Collider player)
+    {
+
+        if (player.gameObject.tag == "Player")
         {
 
-            if (player.gameObject.tag == "Player")
-            {
+            //player.transform.SetParent(peixe.transform);
+            player.transform.parent = peixe.transform;
+            player.GetComponent<playermove>().enabled = false;
+            anim.SetBool("aparece", false);
+            player.GetComponent<Animator>().SetBool("fall",true);
 
-                
-                anim.SetTrigger("volta");
-                //peixe.SetActive(true);
-                // peixe.anim.SetBool("olhar", false);
-            }
-
+            //anim.SetBool("volta");
+            //peixe.SetActive(true);
+            // peixe.anim.SetBool("olhar", false);
         }
+
     }
+
+
 }
 

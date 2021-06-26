8acc8facbfc1fbb8be1d26842cd9e9311eacc6ee
Wine Silva <winny.silva@yahoo.com.br>
2021-06-25 21:39:36 -0300

    scripts de network

---
+++ Assets/GUI/LobbyScreenController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class LobbyScreenController : MonoBehaviour
+{
+
+    public TMP_InputField sala;
+    public TMP_InputField nickname;
+    public GameObject iniciarPartida;
+    public GameObject aguardeMsg;
+    public GameObject infoConexao;
+
+    void Start()
+    {
+        NetworkController.ServerStarted += ServerStarted;
+        iniciarPartida.SetActive(false);
+        aguardeMsg.SetActive(false);
+        infoConexao.SetActive(true);
+
+    }
+
+    void OnDestroy()
+    {
+        NetworkController.ServerStarted -= ServerStarted;
+    }
+
+    public void IniciarHost()
+    {
+        string sala = this.sala.text;
+        string nickname = this.nickname.text;
+        NetworkController.Instance.IniciarHost(sala, nickname);
+    }
+
+    public void IniciarCliente()
+    {
+        string sala = this.sala.text;
+        string nickname = this.nickname.text;
+        
+        NetworkController.Instance.IniciarClient(sala, nickname);
+    }
+        
+    public void ComecarJogo()
+    {
+        aguardeMsg.SetActive(true);
+        NetworkController.Instance.SwitchScene("SampleSceneNetwork");     
+       
+    }
+
+    public void ConectadoComoCliente()
+    {
+        iniciarPartida.SetActive(false);
+        aguardeMsg.SetActive(true);
+        infoConexao.SetActive(false);
+    }
+
+    private void ServerStarted()
+    {
+        iniciarPartida.SetActive(true);
+        aguardeMsg.SetActive(false);
+        infoConexao.SetActive(false);
+    }
+
+
+}
+
+
